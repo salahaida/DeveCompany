456f2b6732c21227ab8672979d0caf0343066cca
SalahAida <salahfordeveloper@gmail.com>
2022-02-11 14:23:18 +0400

    Edit Key The Projects

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5rtmm@hv96j@@(cbdtk$)=t15@p3=3b8g=(c7noskga7@iqu1z'
+SECRET_KEY = 'django-insecure-5rtmm@hvc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ sstarted.bat
@@ -0,0 +1,6 @@
+cd ..\Scripts\activate
+cd ..\src
+git remote add origin https://github.com/salahaida/DeveCompany.git
+git branch -M master
+git push origin master
+code .
