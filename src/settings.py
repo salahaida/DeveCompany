1fc756672885d043001145c3c2f96d9ea7aea2a4
SalahAida <salahfordeveloper@gmail.com>
2022-02-13 04:14:07 +0400

    x

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5rtmm@hvc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
